1184739b0386678ea28e350782fb67a7426adbd2
HanduSaksham <academic.rahul@gmail.com>
2017-07-05 03:06:20 +0530

    Broadcast Receiver -> Static And Dynamic BR

---
+++ L13BroadcastReceivers/app/src/main/java/l12/cb/com/l13broadcastreceivers/MainActivity.java
@@ -0,0 +1,69 @@
+package l12.cb.com.l13broadcastreceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    FrameLayout fl;
+    IntentFilter ifilter;
+    MyReciever myReciever;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myReciever = new MyReciever();
+        fl = (FrameLayout) findViewById(R.id.fl);
+        ifilter = new IntentFilter();
+        ifilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        ifilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(myReciever, ifilter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(myReciever);
+
+    }
+
+    class MyReciever extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+
+                fl.setBackgroundColor(Color.RED);
+
+            }
+
+            if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+
+                fl.setBackgroundColor(Color.GREEN);
+
+            }
+
+
+        }
+
+    }
+
+
+}
---
+++ L13BroadcastReceivers/app/src/main/java/l12/cb/com/l13broadcastreceivers/MyPowerReceiver.java
@@ -0,0 +1,26 @@
+package l12.cb.com.l13broadcastreceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyPowerReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+        if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+
+            Toast.makeText(context, "POWER CONNECTED", Toast.LENGTH_SHORT).show();
+
+        }
+        if(intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)){
+
+            Toast.makeText(context, "POWER DISCONNECTED", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
