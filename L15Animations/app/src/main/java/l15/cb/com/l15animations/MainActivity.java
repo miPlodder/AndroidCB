5c68534561697be5df2407b09914673f618c4a14
HanduSaksham <academic.rahul@gmail.com>
2017-07-11 00:47:59 +0530

    Animations in Android

---
+++ L15Animations/app/src/main/java/l15/cb/com/l15animations/MainActivity.java
@@ -0,0 +1,26 @@
+package l15.cb.com.l15animations;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.iv);
+        iv.setBackgroundResource(R.drawable.cricket_animation);
+
+        AnimationDrawable ar = (AnimationDrawable) iv.getBackground();
+        ar.start();
+
+    }
+}
